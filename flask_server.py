eb112bb0af7980aadbc5ea23b486f7b81261f3d8
Админ <iuy.nickolaencko@gmail.com>
2024-04-14 23:04:49 +0100

    create html file

--- flask_server.py
+++ flask_server.py
@@ -1,10 +1,19 @@
-# Flask server
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello Yuliia"
+    return "Hello Vlas"
+
+
+@app.route("/user")
+def getuser():
+    user={
+        "name": "Vlas",
+        "age" : 4
+    }
+    return jsonify(user)
+
 if __name__ == "__main__":
     app.run(debug = True)
